45a6af40065532c8694333e1698b9b618f0996ea
Simon de Haan <simon@praekeltfoundation.org>
2016-04-10 11:05:12 -0700

    first test passing

--- vxapprouter/router.py
+++ vxapprouter/router.py
@@ -1,7 +1,13 @@
 # -*- test-case-name: vxapprouter.tests.test_router -*-
+import json
 
-from vumi.config import ConfigDict, ConfigList, ConfigInt, ConfigText
+from twisted.internet.defer import inlineCallbacks
+
+from vumi import log
 from vumi.dispatchers.endpoint_dispatchers import Dispatcher
+from vumi.components.session import SessionManager
+from vumi.config import ConfigDict, ConfigList, ConfigInt, ConfigText
+from vumi.message import TransportUserMessage
 
 
 class ApplicationDispatcherConfig(Dispatcher.CONFIG_CLASS):
@@ -11,7 +17,8 @@ class ApplicationDispatcherConfig(Dispatcher.CONFIG_CLASS):
         ("Maximum amount of time in seconds to keep session data around. "
          "Defaults to 5 minutes."),
         default=5 * 60, static=True)
-
+    redis_manager = ConfigDict(
+        "Redis client configuration.", default={}, static=True)
     # Dynamic, per-message configuration
     menu_title = ConfigDict(
         "Content for the menu title",
@@ -30,21 +37,151 @@ class ApplicationDispatcherConfig(Dispatcher.CONFIG_CLASS):
                  "Please try and dial the line again."))
 
 
+class StateResponse(object):
+    def __init__(self, state, session_update=None, inbound=(), outbound=()):
+        self.next_state = state
+        self.session_update = session_update or {}
+        self.inbound = inbound
+        self.outbound = outbound
+
+
+def mkmenu(options, start=1, format='%s) %s'):
+    items = [format % (idx, opt) for idx, opt in enumerate(options, start)]
+    return '\n'.join(items)
+
+
 class ApplicationDispatcher(Dispatcher):
     CONFIG_CLASS = ApplicationDispatcherConfig
-    worker_name = None
+    worker_name = 'application_dispatcher'
 
+    STATE_START = "start"
+    STATE_SELECT = "select"
+    STATE_SELECTED = "selected"
+    STATE_BAD_INPUT = "bad_input"
+
+    def setup_dispatcher(self):
+        self.handlers = {
+            self.STATE_START: self.handle_state_start,
+            # self.STATE_SELECT: self.handle_state_select,
+            # self.STATE_SELECTED: self.handle_state_selected,
+            # self.STATE_BAD_INPUT: self.handle_state_bad_input,
+        }
+        return super(ApplicationDispatcher, self).setup_dispatcher()
+
+    def session_manager(self, config):
+        return SessionManager.from_redis_config(
+            config.redis_manager,
+            key_prefix=self.worker_name,
+            max_session_length=config.session_expiry)
+
+    def handle_state_start(self, config, session, msg):
+        """
+        When presenting the menu, we also store the list of endpoints
+        in the session data. Later, in the select state, we load
+        these endpoints and retrieve the candidate endpoint based
+        on the user's menu choice.
+        """
+        reply_msg = msg.reply(self.create_menu(config))
+        endpoints = json.dumps(
+            [entry['endpoint'] for entry in config.entries]
+        )
+        return StateResponse(
+            self.STATE_SELECT, {'endpoints': endpoints}, outbound=[reply_msg])
+
+    # def handle_state_select(self, config, session, msg):
+    #     endpoint = self.get_endpoint_for_choice(msg, session)
+    #     if endpoint is None:
+    #         reply_msg = msg.reply(config.invalid_input_message)
+    #         return StateResponse(self.STATE_BAD_INPUT, outbound=[reply_msg])
+    #
+    #     if endpoint not in self.target_endpoints(config):
+    #         log.msg(("Router configuration change forced session "
+    #                  "termination for user %s" % msg['from_addr']))
+    #         error_reply_msg = self.make_error_reply(msg, config)
+    #         return StateResponse(None, outbound=[error_reply_msg])
+    #
+    #     forwarded_msg = self.forwarded_message(
+    #         msg, content=None,
+    #         session_event=TransportUserMessage.SESSION_NEW)
+    #     log.msg("Switched to endpoint '%s' for user %s" %
+    #             (endpoint, msg['from_addr']))
+    #     return StateResponse(
+    #         self.STATE_SELECTED, {'active_endpoint': endpoint},
+    #         inbound=[(forwarded_msg, endpoint)])
+    #
+    # def handle_state_selected(self, config, session, msg):
+    #     active_endpoint = session['active_endpoint']
+    #     if active_endpoint not in self.target_endpoints(config):
+    #         log.msg(("Router configuration change forced session "
+    #                  "termination for user %s" % msg['from_addr']))
+    #         error_reply_msg = self.make_error_reply(msg, config)
+    #         return StateResponse(None, outbound=[error_reply_msg])
+    #     else:
+    #         return StateResponse(
+    #             self.STATE_SELECTED, inbound=[(msg, active_endpoint)])
+    #
+    # def handle_state_bad_input(self, config, session, msg):
+    #     choice = self.get_menu_choice(msg, (1, 1))
+    #     if choice is None:
+    #         reply_msg = msg.reply(config.invalid_input_message)
+    #         return StateResponse(self.STATE_BAD_INPUT, outbound=[reply_msg])
+    #     else:
+    #         return self.handle_state_start(config, session, msg)
+
+    def create_menu(self, config):
+        labels = [entry['label'] for entry in config.entries]
+        return (config.menu_title['content'] + "\n" + mkmenu(labels))
+
+    def make_error_reply(self, msg, config):
+        return msg.reply(config.error_message, continue_session=False)
+
+    @inlineCallbacks
     def process_inbound(self, config, msg, connector_name):
-        target = self.find_target(config, msg, connector_name)
-        if target is None:
+        log.msg("Processing inbound message: %s" % (msg,))
+        user_id = msg['from_addr']
+        session_manager = yield self.session_manager(config)
+        session = yield session_manager.load_session(user_id)
+        session_event = msg['session_event']
+        if not session or session_event == TransportUserMessage.SESSION_NEW:
+            log.msg("Creating session for user %s" % user_id)
+            session = {}
+            state = self.STATE_START
+            yield session_manager.create_session(user_id, state=state)
+        elif session_event == TransportUserMessage.SESSION_CLOSE:
+            yield self.handle_session_close(config, session, msg)
             return
-        return self.publish_inbound(msg, target[0], target[1])
+        else:
+            log.msg("Loading session for user %s: %s" % (user_id, session,))
+            state = session['state']
+
+        try:
+            # We must assume the state handlers might be async, even if the
+            # current implementations aren't. There is at least one test that
+            # depends on asynchrony here to hook into the state transition.
+            state_resp = yield self.handlers[state](config, session, msg)
+            if state_resp.next_state is None:
+                # Session terminated (right now, just in the case of a
+                # administrator-initiated configuration change
+                yield session_manager.clear_session(user_id)
+            else:
+                session['state'] = state_resp.next_state
+                session.update(state_resp.session_update)
+                if state != state_resp.next_state:
+                    log.msg("State transition for user %s: %s => %s" %
+                            (user_id, state, state_resp.next_state))
+                yield session_manager.save_session(user_id, session)
+            for msg, endpoint in state_resp.inbound:
+                yield self.publish_inbound(msg, connector_name, endpoint)
+            for msg in state_resp.outbound:
+                yield self.process_outbound(config, msg, connector_name)
+        except:
+            log.err()
+            yield session_manager.clear_session(user_id)
+            yield self.publish_outbound(self.make_error_reply(msg, config))
 
     def process_outbound(self, config, msg, connector_name):
-        target = self.find_target(config, msg, connector_name)
-        if target is None:
-            return
-        return self.publish_outbound(msg, target[0], target[1])
+        return self.publish_outbound(
+            msg, connector_name, msg.get_routing_endpoint())
 
     def process_event(self, config, event, connector_name):
         target = self.find_target(config, event, connector_name)
--- vxapprouter/tests/test_router.py
+++ vxapprouter/tests/test_router.py
@@ -1,9 +1,16 @@
-from vumi.tests.helpers import VumiTestCase
+from vumi.components.session import SessionManager
 from vumi.dispatchers.tests.helpers import DispatcherHelper
+from vumi.tests.helpers import VumiTestCase, PersistenceHelper
+from vumi.tests.utils import LogCatcher
 
-from twisted.internet.defer import inlineCallbacks
+from twisted.internet.defer import inlineCallbacks, succeed
+from twisted.python.failure import Failure
 
 from vxapprouter.router import ApplicationDispatcher
+
+
+class DummyError(Exception):
+    """Custom exception to use in test cases."""
 
 
 class TestApplicationRouter(VumiTestCase):
@@ -21,9 +28,27 @@ class TestApplicationRouter(VumiTestCase):
         "receive_outbound_connectors": ["app1", "app2"],
     }
 
+    @inlineCallbacks
     def setUp(self):
+        self.persistence_helper = self.add_helper(PersistenceHelper())
+        self.redis = yield self.persistence_helper.get_redis_manager()
+        self.session_manager = SessionManager(self.redis)
+        self.patch(
+            ApplicationDispatcher, 'session_manager',
+            lambda *a: succeed(self.session_manager))
         self.disp_helper = self.add_helper(
             DispatcherHelper(ApplicationDispatcher))
+
+    def ch(self, connector_name):
+        return self.disp_helper.get_connector_helper(connector_name)
+
+    def assert_rkeys_used(self, *rkeys):
+        broker = self.disp_helper.worker_helper.broker
+        self.assertEqual(set(rkeys), set(broker.dispatched['vumi'].keys()))
+
+    def assert_dispatched_endpoint(self, msg, endpoint, dispatched_msgs):
+        msg.set_routing_endpoint(endpoint)
+        self.assertEqual([msg], dispatched_msgs)
 
     def get_dispatcher(self, **config_extras):
         config = self.ROUTER_CONFIG.copy()
@@ -31,7 +56,17 @@ class TestApplicationRouter(VumiTestCase):
         return self.disp_helper.get_dispatcher(config)
 
     @inlineCallbacks
-    def test_something(self):
-        d = yield self.get_dispatcher()
-        print d
-        assert True
+    def test_first_inbound_message_routing(self):
+        yield self.get_dispatcher()
+        msg = yield self.ch("transport").make_dispatch_inbound("inbound")
+        self.assert_rkeys_used('transport.inbound', 'transport.outbound')
+        [reply] = self.ch('transport').get_dispatched_outbound()
+        self.assertEqual(
+            reply['content'],
+            '\n'.join([
+                "Please select a choice.",
+                "1) Flappy Bird",
+            ]))
+
+        session = yield self.session_manager.load_session(msg['from_addr'])
+        self.assertEqual(session['state'], ApplicationDispatcher.STATE_SELECT)
